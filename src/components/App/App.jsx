cc720f44a7f4996ba52dcf2733eca03bed1a3ac5
Vladyslav Venher <vladyslavvenher@gmail.com>
2024-09-01 19:12:06 +0300

    do h/w

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,49 +1,88 @@
 import { useState, useEffect } from 'react';
-import Description from '../Description/Description';
-import Feedback from '../Feedback/Feedback';
-import Options from '../Options/Options';
-import Notification from '../Notification/Notification';
+import SearchBar from '../SearchBar/SearchBar';
+import ImageGallery from '../ImageGallery/ImageGallery';
+import Loader from '../Loader/Loader';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import LoadMoreBtn from '../LoadMoreBtn/LoadMoreBtn';
+import ImageModal from '../ImageModal/ImageModal';
+import { fetchImages } from '../../services/api';
+import toast, { Toaster } from 'react-hot-toast';
 import css from './App.module.css';
 
 export default function App() {
-  const [feedback, setFeedback] = useState(() => {
-    const savedFeedback = localStorage.getItem('feedback');
-    return savedFeedback
-      ? JSON.parse(savedFeedback)
-      : { good: 0, neutral: 0, bad: 0 };
-  });
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [error, setError] = useState(null);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(true);
 
   useEffect(() => {
-    window.localStorage.setItem('feedback', JSON.stringify(feedback));
-  }, [feedback]);
+    if (query === '') {
+      return;
+    }
 
-  const updateFeedback = feedbackType => {
-    setFeedback(prevFeedback => ({
-      ...prevFeedback,
-      [feedbackType]: prevFeedback[feedbackType] + 1,
-    }));
+    async function getImages() {
+      try {
+        setLoading(true);
+        setError(false);
+        const res = await fetchImages(query, page);
+        setImages(prevImages => [...prevImages, ...res]);
+        if (res.length < 10) {
+          setTotalPages(false);
+        }
+      } catch (error) {
+        setError(error.message);
+        toast.error('Failed to fetch images.');
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    getImages();
+  }, [query, page]);
+
+  const handleSearchSubmit = newQuery => {
+    setQuery(newQuery);
+    setImages([]);
+    setPage(1);
+    setError(null);
+    setTotalPages(true);
   };
 
-  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
-  const positiveFeedback = Math.round((feedback.good / totalFeedback) * 100);
+  const handleLoadMore = () => {
+    setPage(page + 1);
+  };
+
+  const openModal = largeImageURL => {
+    setSelectedImage(largeImageURL);
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+    setSelectedImage(null);
+  };
 
   return (
     <div className={css.container}>
-      <Description />
-      <Options
-        updateFeedback={updateFeedback}
-        setFeedback={setFeedback}
-        totalFeedback={totalFeedback}
-      />
-      {totalFeedback > 0 ? (
-        <Feedback
-          feedback={feedback}
-          totalFeedback={totalFeedback}
-          positiveFeedback={positiveFeedback}
-        />
-      ) : (
-        <Notification message="No feedback yet" />
+      <SearchBar onSubmit={handleSearchSubmit} />
+      {images.length > 0 && (
+        <ImageGallery images={images} onImageClick={openModal} />
       )}
+      {!totalPages && <b>END OF COLLECTION!!!</b>}
+      {error && <ErrorMessage message={error} />}
+      {loading && <Loader />}
+      {images.length > 0 && !loading && totalPages && (
+        <LoadMoreBtn onClick={handleLoadMore} />
+      )}
+
+      {showModal && (
+        <ImageModal largeImage={selectedImage} onClose={closeModal} />
+      )}
+      <Toaster />
     </div>
   );
 }
